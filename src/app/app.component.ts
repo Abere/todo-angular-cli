9f023b5b469d4602611d77be38e109972ebd0355
ezedinff <ezedin.fedlu@gmail.com>
2018-02-19 01:40:12 +0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  newTodo: string;
+  todos: any;
+  todoObj: any;
+  constructor(){
+    this.newTodo = '';
+    this.todos = [];
+  }
+  addTodo(event) {
+    this.todoObj = {
+      newTodo: this.newTodo,
+        completed: false
+    };
+    this.todos.push(this.todoObj);
+    this.newTodo = '';
+    event.preventDefault();
+    console.log("hello");
+  }
+
+  deleteTodo(index){
+    this.todos.splice(index, 1);
+  }
+  deleteSelectedTodos() {
+    for (let i = (this.todos.length - 1); i > -1; i--) {
+      if (this.todos[i].completed) {
+        this.todos.splice(i, 1);
+      }
+    }
+  }
+}
